a2216e71395a7cd8e58c3a0aafbe263acb4b2681
raghavjnvk <96766510+raghavjnvk@users.noreply.github.com>
2022-05-15 18:40:13 +0530

    Skyscraper.java
    Just created template of Skyscraper.java file

---
+++ Skyscraper.java
@@ -0,0 +1,18 @@
+package com.greatlearning.lab2;
+
+public class Skyscraper
+{
+	// You are required to print N lines. Print the size of the floor in descending order
+	
+	public void floorConstruction()
+	{
+	
+		int size=0, largeSizeNum=0, smallSizeNum=0, counter=0, inputs=0;
+	
+	}
+
+}
+
+
+
+
