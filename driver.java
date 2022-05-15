86855ddd80e7cf20c7795f4da0a4646086a3b60c
raghavjnvk <96766510+raghavjnvk@users.noreply.github.com>
2022-05-15 18:41:21 +0530

    Skyscraper.java
    Created driver class for Skyscraper.

---
+++ driver.java
@@ -0,0 +1,11 @@
+//Created driver class for Skyscraper
+package com.greatlearning.lab2;
+
+public class driver
+{
+public static void main(String args[])
+{
+	
+	System.out.println("Sizing the floors in descending order");
+}
+}
